74c75e8c939de78e9c83174b2a0a3ec21b0b0b0e
Ernesto Lopez <ernestodaniellopez504@fpuna.edu.py>
2023-03-27 21:02:02 -0400

    save

---
+++ clase1/untitled/src/ClaseMath.java
@@ -0,0 +1,12 @@
+
+public class ClaseMath {
+    int valorAbs = Math.abs(-3);
+    double max = Math.max(3.5, 4);
+    double min = Math.min(3.5, 9.9);
+    //redondear
+    double arriba = Math.ceil(3.5);
+    double abajo = Math.floor(3.5);
+    //redondear si es n.5 hacia arriba sino hacia abajo
+    double redond = Math.round(3.6);
+    double random = Math.random();//numero aleatorio entre 0 y 1
+}
---
+++ clase1/untitled/src/ClaseRandom.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class ClaseRandom {
+    //utiliza el metodo random de la clase Match 
+    //es mucho mas flexible para generar numeros enteros y decimales aleatorios
+    Random  randomObj = new Random();
+    int randomInt = randomObj.nextInt(0);
+}
